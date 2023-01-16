20a705517e1c096b20cd3f911ba9d3e5bce4fc5f
airpark69 <50907018+airpark69@users.noreply.github.com>
2023-01-16 23:23:36 +0900

    20230116-selenium-fix

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # S3Project
 
+## docker 빌드 환경
+
 .env 내의 환경변수를 시스템 환경변수 편집에서 추가해줘야 작동합니다.
+
+.env 내에다가 CHROME_DRIVER_PATH=/usr/src/chrome/chromedriver 를 추가해줘야 합니다.
 
 2023-01-05 기준 문제점
 
@@ -10,7 +14,15 @@
 
 -> 그냥 postgres를 지정해주면서 해결
 
+2023-01-16 기준
+
+- [해결]docker 빌드 시, insta_Crawl 이 webdriver를 위치를 제대로 잡지 못함
+
+-> linux 환경에 따른 별도의 webdriver option 지정이 필요했다.
+
+-> 동적 스크래핑이 docker 내부에서도 제대로 작동하는 것을 확인.
+
 - trend_score 가 corpus 내의 단어에 대해서 입력값을 제대로 반영하지 못하는 부분
-- docker 빌드 시, insta_Crawl 이 webdriver를 위치를 제대로 잡지 못함
+
 - docker volume을 활용하여 corpus와 api_results를 postgreSQL 이미지끼리 공유할 수 있도록 추가해야함
  
--- app/functions/instaCrawl.py
+++ app/functions/instaCrawl.py
@@ -19,11 +19,25 @@ from . import database
 #### config를 내부에 넣고, 환경변수로 관리하는 방식
 insta_email = os.getenv('INSTA_EMAIL')
 insta_password = os.getenv('INSTA_PASSWORD')
+chrome_path = os.getenv('CHROME_DRIVER_PATH')
 
 INSTA_CONFIG = {
     'email':insta_email,
     'password':insta_password
 }
+
+### webdriver option값들
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+options.add_argument('window-size=1920x1080')
+options.add_argument("disable-gpu")
+options.add_argument(f'user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36')
+# linux 환경에서 필요한 option
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+####
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
+
 
 # corpus 폴더 내 파일 존재여부 확인
 def check_txt(category, dt_now):
@@ -139,14 +153,9 @@ def login(id, pw, driver):
 # 요청시 word에 해당하는 태그로 인스타검색 후 결과반환
 def get_results(word, num=10):
     # 크롤링 시작
-    options = webdriver.ChromeOptions()
-    options.add_argument('headless')
-    options.add_argument('window-size=1920x1080')
-    options.add_argument("disable-gpu")
-    options.add_argument(f'user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36')
-    options.add_experimental_option("excludeSwitches", ["enable-logging"])
+    
     # 크롬 브라우저 열기
-    driver = webdriver.Chrome('chrome/chromedriver.exe', chrome_options=options)
+    driver = webdriver.Chrome(chrome_path, chrome_options=options)
 
     driver.get('https://www.instagram.com/accounts/login/')
     time.sleep(3)
@@ -179,14 +188,8 @@ def get_corpus(word_list, num=10):
     expected_time = 13 + (num * 3) * len(word_list)
     dt_now = datetime.datetime.now().date()
     print(f"예상소요시간 : {expected_time}초")
-    options = webdriver.ChromeOptions()
-    options.add_argument('headless')
-    options.add_argument('window-size=1920x1080')
-    options.add_argument("disable-gpu")
-    options.add_argument(f'user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36')
-    options.add_experimental_option("excludeSwitches", ["enable-logging"])
     # 크롬 브라우저 열기
-    driver = webdriver.Chrome('chromedriver.exe', chrome_options=options)
+    driver = webdriver.Chrome(chrome_path, chrome_options=options)
     driver.get('https://www.instagram.com/accounts/login/')
     time.sleep(3)
 
@@ -228,28 +231,5 @@ def get_corpus(word_list, num=10):
 
 # 하루에 한번, 모든 insta_corpus를 저장합니다.
 def Setting_all_corpus():
-    categorys = ['포기김치', '반찬세트', '장아찌', '절임류', '기타반찬류', '단무지', '조림류', '장조림', '볶음류',
-       '젓갈장류', '된장', '김해초', '과일', '견과류', '채소', '쌀', '잡곡혼합곡', '건과류', '쇠고기',
-       '닭고기', '돼지고기', '알류', '축산가공식품', '기타육류', '양고기', '오리고기', '한방재료',
-       '건강분말', '건강즙과일즙', '꿀', '인삼', '홍삼', '건강환정', '영양제', '비타민제',
-       '환자식영양보충식', '꽁치고등어', '골뱅이번데기', '햄', '참치연어', '기타통조림캔', '황도과일', '생선',
-       '해산물어패류', '피클올리브', '건어물', '차류', '주스과즙음료', '전통차음료', '건강기능성음료',
-       '우유요구르트', '커피', '청량탄산음료', '코코아', '두유', '탄산수', '어묵', '기타조미료', '고추장',
-       '별미김치', '백김치', '파김치', '갓김치', '열무김치', '깍두기', '절임배추', '액젓', '옥수수콩',
-       '면류', '총각김치', '세트', '겉절이', '물엿올리고당', '즉석국즉석탕', '기타소스드레싱',
-       '기타냉동간편조리식품', '카레짜장', '스낵', '가루분말류', '오이소박이', '튀김류', '수산가공식품',
-       '채식푸드', '양념장', '곤약', '간장', '가공안주류', '초콜릿', '떡', '한과', '쿠키',
-       '아이스크림빙수', '빵', '강정', '팝콘강냉이류', '시리얼', '젤리', '전병', '엿', '푸딩', '사탕',
-       '화과자', '껌', '만두', '기타장류', '식초', '라면', '단백질보충제', '기타다이어트식품', '굴소스',
-       '청국장', '쌈장', '메주', '누룽지', '즉석밥', '샐러드', '스프', '죽', '간식디저트', '떡볶이',
-       '피자', '핫도그', '딤섬', '도시락', '햄버거', '고춧가루', '소금', '고추냉이', '겨자', '설탕',
-       '후추', '천연감미료', '마요네즈', '오리엔탈드레싱', '스파게티파스타소스', '머스타드소스', '칠리핫소스',
-       '케첩', '돈가스소스', '스테이크바베큐소스', '발사믹드레싱', '딸기잼', '기타잼시럽', '치즈', '마가린',
-       '버터', '생크림', '휘핑크림', '연유', '콜라겐', '가르시니아', '히알루론산', '식이섬유',
-       '다이어트차', '구이', '찌개국', '맛살게살', '함박미트볼', '식용유오일', '제과제빵재료', '막걸리탁주',
-       '약주', '소주', '일반증류주', '리큐르주', '기타주류', '전통주선물세트', '과실주', '생수', '케이크',
-       '기타과자', '대상별', '기능별', '비타민', '유산균', '도시락밥류', '샐러드닭가슴살', '죽스프',
-       '만두딤섬', '떡볶이튀김어묵', '피자핫도그햄버거', '기타간편조리식품', '베이커리', '젤리캐러멜푸딩',
-       '사탕껌엿', '과자쿠키', '팝콘강냉이', '전통과자', '제로음료', '우유요거트', '파우더스무디', '면파스타',
-       '밥죽', '볶음튀김', '조림찜']
+    categorys = ['피자', '햄버거', '치킨', '케이크', '커피', '라떼']
     get_corpus(categorys)
